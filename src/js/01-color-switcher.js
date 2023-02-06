4cabe7de9cbd2eda3dd3e1ef2c5d2937a62bf503
Veronica Soloviova <nika.sv.work@gmail.com>
2023-02-06 22:17:57 +0200

    adds color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+}
 
+let intervalId = null;
+refs.stopBtn.disabled = true; 
+
+refs.startBtn.addEventListener('click', onStartBtnClick)
+refs.stopBtn.addEventListener('click', onStopBtnClick)
+
+function onStartBtnClick() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    refs.stopBtn.disabled = false;
+    refs.startBtn.disabled = true;
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onStopBtnClick() {
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
